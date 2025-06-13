661ea885ad7c95254fe235776227645e98018d69
Mendy Ostrecher <Mendy9271@gmail.com>
2025-06-12 20:28:40 -0400

    shorter code

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-const _ =require('lodash');
-
-const number =[2, 4, 2, 4, 5, 5, 4, 8, 7, 8 ,9]
-const uniqe = _.uniq(number)
-console.log(uniqe)
+const lodash = require('lodash');
+const number = [1, 2, 2, 3, 3, 4, 4, 5, 5]
+console.log(lodash.uniq(number))
